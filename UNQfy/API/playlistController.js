2680e56d958acfe779d1e1d8fc06091e07d71637
Cristian Gonzalez <cristian.gonzalez.unq@gmail.com>
2021-06-20 09:37:58 -0300

    playlistDoblePost

--- UNQfy/API/playlistController.js
+++ UNQfy/API/playlistController.js
@@ -36,7 +36,7 @@ playlists.get(root, function (req, res) {
         //Levantar error
     }
 });
-
+/*
 // POST : Crea una playlist con máxima duración y tracks pertenecientes a alguno de los géneros.
 playlists.post(root, function (req, res) {
     try {
@@ -58,6 +58,57 @@ playlists.post(root, function (req, res) {
     }
     catch {
         //Levantar error
+    }
+});*/
+
+
+playlists.post(root, function (req, res) {
+    try {
+        let unqfy = validate.getUNQfy();
+        if (req.body.genres !== undefined && req.body.maxDuration !== undefined && req.body.name !== undefined) {
+            console.log("entrepormaxduration")
+            let unqfy = validate.getUNQfy();
+            let maxDuration = req.body.maxDuration;
+            let name = req.body.name;
+            let genres = req.body.genres;
+            unqfy.createPlaylist(name, genres, maxDuration);
+            let playlist = unqfy.searchPlaylistByName(name).filter(elem => elem.name.toLowerCase() === name.toLowerCase());
+            res.status(201);
+            res.send(
+                {
+                    "id": playlist[0].id,
+                    "name": playlist[0].name,
+                    "duration": playlist[0].actualDuration, //o maxDuration?
+                    "tracks": playlist[0].tracks
+                }
+            );
+        }
+        else if (req.body.tracks !== undefined && req.body.name !== undefined) {
+            console.log("entreportracks")
+            let name = req.body.name;
+            let temas = req.body.tracks
+            let tracks = unqfy.getTracksFromListOfIdTracks(temas)
+            let maxDuration = unqfy.getDurationFromListOfTracks(tracks);
+            let genres = unqfy.getGenresFromListOfTracks(tracks);
+            unqfy.createPlaylist(name, genres, maxDuration);
+            let playlist = unqfy.searchPlaylistByName(name).filter(elem => elem.name.toLowerCase() === name.toLowerCase());
+            res.status(201);
+            res.send(
+                {
+                    "id": playlist[0].id,
+                    "name": playlist[0].name,
+                    "duration": playlist[0].actualDuration, //o maxDuration?
+                    "tracks": playlist[0].tracks
+                }
+            );
+        }
+        else {
+            res.status(404)
+            res.send("los datos ingresados son insuficientes");
+        }
+    }
+    catch {
+        res.json(error.name)
     }
 });
 
